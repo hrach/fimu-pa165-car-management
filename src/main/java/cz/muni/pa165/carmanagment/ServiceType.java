ddeb58f9f4039fb758d6df2cb3e97ac7a7b06993
tomasbobek <tomasbobek@Tomas-MacBook-Pro.local>
2013-10-10 20:05:44 +0200

    Added entity ServiceType

---
+++ src/main/java/cz/muni/pa165/carmanagment/ServiceType.java
@@ -0,0 +1,93 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.pa165.carmanagment;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author tomasbobek
+ */
+
+@Entity
+public class ServiceType {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String name;
+    
+    @OneToMany(mappedBy = "servicetype", cascade = CascadeType.ALL)
+    private List<ServiceInterval> services = new ArrayList<ServiceInterval>();
+    
+    public ServiceType () {
+        
+    }
+    
+    public ServiceType(String name) {
+        this.name = name;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    public List<ServiceInterval> getServices() {
+        return services;
+    }
+
+    public void setServices(List<ServiceInterval> services) {
+        this.services = services;
+    }
+    
+    @Override
+    public String toString() {
+        return this.name;
+    }
+    
+    @Override
+    public int hashCode() {
+        return (this.id != null ? this.id.hashCode() : 0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ServiceType other = (ServiceType) obj;
+        if (this.id == null || !this.id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+}
