c8dc76d9387a1e3fcd8486299263558f8dce08dc
tomasbobek <tomasbobek@Tomas-MacBook-Pro.local>
2013-10-10 20:58:08 +0200

    Added main drafts of specific DAO classes

---
+++ src/main/java/cz/muni/pa165/carmanagment/VehicleDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.pa165.carmanagment;
+
+/**
+ *
+ * @author tomasbobek
+ */
+public class VehicleDao extends GeneralDao<Long, Vehicle> {
+    
+    public VehicleDao() {
+        super();
+    }
+    
+}
