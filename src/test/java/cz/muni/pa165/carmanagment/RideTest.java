3ae5155b9023da9075044dcc99117cac86eed875
hrach <hrach.cz@gmail.com>
2013-10-11 17:53:42 +0200

    Added Ride entity tests.

---
+++ src/test/java/cz/muni/pa165/carmanagment/RideTest.java
@@ -0,0 +1,62 @@
+
+package cz.muni.pa165.carmanagment;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNull;
+import junit.framework.TestCase;
+
+
+/**
+ * Ride entity test.
+ * @author skrasek
+ */
+public class RideTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+
+    public void testRideLenght()
+    {
+        Ride r = new Ride();
+        r.setTachometerStart(new Long(23));
+        r.setTachometerEnd(new Long(34));
+        
+        assertEquals(new Long(23), r.getTachometerStart());
+        assertEquals(new Long(11), r.getRideLength());
+    }
+
+    public void testRideIds()
+    {
+        Ride n = new Ride();
+        
+        assertNull(n.getId());
+
+        Ride one = new Ride();
+        Ride two = new Ride();
+        
+        one.setId(new Long(1));
+        two.setId(new Long(1));
+        
+        assertEquals(one, two);
+        
+        two.setId(null);
+        
+        assertFalse(one.equals(two));
+        assertFalse(two.equals(one));
+        
+        two.setId(new Long(2));
+        
+        assertFalse(one.equals(two));
+        assertFalse(two.equals(one));
+    }
+    
+}
