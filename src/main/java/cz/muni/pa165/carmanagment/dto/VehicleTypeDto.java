8ca1f1a1c08eda2d7a27d6cd6f7a946783c717dd
Petr Zvonicek <zvonicek@gmail.com>
2013-10-27 22:03:28 +0100

    VehicleTypeDto: added hashCode and equals

--- src/main/java/cz/muni/pa165/carmanagment/dto/VehicleTypeDto.java
+++ src/main/java/cz/muni/pa165/carmanagment/dto/VehicleTypeDto.java
@@ -1,7 +1,8 @@
 package cz.muni.pa165.carmanagment.dto;
 
 /**
- *
+ * VehicleType data transfer object
+ * 
  * @author zvonicek
  */
 public class VehicleTypeDto
@@ -40,5 +41,29 @@ public class VehicleTypeDto
     public void setMaxKm(Long maxKm)
     {
         this.maxKm = maxKm;
+    }        
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 3;
+        hash = 43 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final VehicleTypeDto other = (VehicleTypeDto) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
     }        
 }
