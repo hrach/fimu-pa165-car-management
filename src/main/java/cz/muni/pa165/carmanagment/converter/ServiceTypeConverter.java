051816ef998949297e50d662f1422a88ff5abe52
Petr Zvonicek <zvonicek@gmail.com>
2013-10-30 17:15:27 +0100

    Implemented ServiceTypeConverter, VehicleConverter

--- src/main/java/cz/muni/pa165/carmanagment/converter/ServiceTypeConverter.java
+++ src/main/java/cz/muni/pa165/carmanagment/converter/ServiceTypeConverter.java
@@ -8,19 +8,47 @@ package cz.muni.pa165.carmanagment.converter;
 
 import cz.muni.pa165.carmanagment.dto.ServiceTypeDto;
 import cz.muni.pa165.carmanagment.model.ServiceType;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author Jakub Marecek <xmarec at gmail.com>
+ * @author zvonicek
  */
 class ServiceTypeConverter {
 
-    static ServiceTypeDto entityToDto(ServiceType serviceType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    static ServiceTypeDto entityToDto(ServiceType entity) {
+        if (entity == null)
+            throw new IllegalArgumentException("attribute entity is null");
+        
+        ServiceTypeDto dto = new ServiceTypeDto();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        
+        return dto;      
     }
 
-    static ServiceType dtoToEntity(ServiceTypeDto serviceType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    static ServiceType dtoToEntity(ServiceTypeDto dto) {
+        if (dto == null)
+            throw new IllegalArgumentException("attribute dto is null");
+        
+        ServiceType entity = new ServiceType();
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+        
+        return entity;     
     }
     
+
+    public static List<ServiceTypeDto> entityToDto(List<ServiceType> entities) {
+        if (entities == null)
+            throw new IllegalArgumentException("attribute entities is null");
+        
+        List<ServiceTypeDto> list = new ArrayList<ServiceTypeDto>();
+        for (ServiceType type : entities) {
+            list.add(ServiceTypeConverter.entityToDto(type));
+        }
+        
+        return list;
+    }        
 }
--- src/main/java/cz/muni/pa165/carmanagment/converter/VehicleConverter.java
+++ src/main/java/cz/muni/pa165/carmanagment/converter/VehicleConverter.java
@@ -8,19 +8,51 @@ package cz.muni.pa165.carmanagment.converter;
 
 import cz.muni.pa165.carmanagment.dto.VehicleDto;
 import cz.muni.pa165.carmanagment.model.Vehicle;
+import cz.muni.pa165.carmanagment.model.VehicleType;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author Jakub Marecek <xmarec at gmail.com>
+ * @author zvonicek
  */
 public class VehicleConverter {
 
-    public static Vehicle dtoToEntity(VehicleDto vehicle) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static Vehicle dtoToEntity(VehicleDto dto) {
+        if (dto == null)
+            throw new IllegalArgumentException("attribute dto is null");
+        
+        Vehicle entity = new Vehicle();
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+        entity.setTchometer(dto.getTachometer());
+        entity.setType(VehicleTypeConverter.dtoToEntity(dto.getType()));
+        
+        return entity;        
     }
 
-    public static VehicleDto entityToDto(Vehicle vehicle) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static VehicleDto entityToDto(Vehicle entity) {
+        if (entity == null)
+            throw new IllegalArgumentException("attribute entity is null");
+        
+        VehicleDto dto = new VehicleDto();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        dto.setTachometer(entity.getTachometer());
+        dto.setType(VehicleTypeConverter.entityToDto(entity.getType()));
+        
+        return dto;    
     }
-    
+
+    public static List<VehicleDto> entityToDto(List<Vehicle> entities) {
+        if (entities == null)
+            throw new IllegalArgumentException("attribute entities is null");
+        
+        List<VehicleDto> list = new ArrayList<VehicleDto>();
+        for (Vehicle vehicle : entities) {
+            list.add(VehicleConverter.entityToDto(vehicle));
+        }
+        
+        return list;
+    }    
 }
