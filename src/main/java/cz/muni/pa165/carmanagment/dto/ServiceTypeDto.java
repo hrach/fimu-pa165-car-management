41ac86ff39276ea1399ea522a5b37e9579daf123
Petr Zvonicek <zvonicek@gmail.com>
2013-10-30 16:55:31 +0100

    Added ServiceType Data Access Object

--- src/main/java/cz/muni/pa165/carmanagment/dto/ServiceTypeDto.java
+++ src/main/java/cz/muni/pa165/carmanagment/dto/ServiceTypeDto.java
@@ -3,13 +3,66 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cz.muni.pa165.carmanagment.dto;
 
 /**
  *
- * @author Jakub Marecek <xmarec at gmail.com>
+ * @author zvonicek
  */
 public class ServiceTypeDto {
+
+    private Long id;
     
+    private String name;
+
+    public ServiceTypeDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public ServiceTypeDto(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + (this.id != null ? this.id.hashCode() : 0);
+        hash = 47 * hash + (this.name != null ? this.name.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ServiceTypeDto other = (ServiceTypeDto) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
 }
