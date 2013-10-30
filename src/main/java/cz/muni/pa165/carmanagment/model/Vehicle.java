58fbd08abefdabe97bd2342e20f1a17a0febddf4
Petr Zvonicek <zvonicek@gmail.com>
2013-10-30 17:37:05 +0100

    Vehicle: added one-many relation vehicle-rides and vehicle-serviceInterfals

--- src/main/java/cz/muni/pa165/carmanagment/model/Vehicle.java
+++ src/main/java/cz/muni/pa165/carmanagment/model/Vehicle.java
@@ -2,6 +2,8 @@
 package cz.muni.pa165.carmanagment.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 
 /**
@@ -32,6 +35,11 @@ public class Vehicle implements Serializable {
     @ManyToOne(cascade = CascadeType.ALL)
     private VehicleType type;
     
+    @OneToMany(mappedBy = "ride", cascade = CascadeType.ALL)
+    private List<Ride> rides = new ArrayList<Ride>();    
+
+    @OneToMany(mappedBy = "serviceInterval", cascade = CascadeType.ALL)
+    private List<ServiceInterval> serviceIntervals = new ArrayList<ServiceInterval>();        
     
     public Vehicle() {
         
@@ -74,6 +82,22 @@ public class Vehicle implements Serializable {
     public void setType(VehicleType type) {
         this.type = type;
     }
+    
+    public List<Ride> getRides() {
+        return rides;
+    }
+
+    public void setRides(List<Ride> rides) {
+        this.rides = rides;
+    }    
+
+    public List<ServiceInterval> getServiceIntervals() {
+        return serviceIntervals;
+    }
+
+    public void setServiceIntervals(List<ServiceInterval> serviceIntervals) {
+        this.serviceIntervals = serviceIntervals;
+    }        
     
     @Override
     public String toString() {
