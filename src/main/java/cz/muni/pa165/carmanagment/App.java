cacf934d40476d597fdf175b3c31310501154b71
Petr Zvonicek <zvonicek@gmail.com>
2013-10-31 10:55:35 +0100

    VehicleTypeService test use with Spring beans

--- src/main/java/cz/muni/pa165/carmanagment/App.java
+++ src/main/java/cz/muni/pa165/carmanagment/App.java
@@ -1,5 +1,13 @@
 package cz.muni.pa165.carmanagment;
 
+import cz.muni.pa165.carmanagment.dao.VehicleTypeDaoImpl;
+import cz.muni.pa165.carmanagment.dto.VehicleTypeDto;
+import cz.muni.pa165.carmanagment.model.VehicleType;
+import cz.muni.pa165.carmanagment.service.VehicleTypeService;
+import cz.muni.pa165.carmanagment.service.VehicleTypeServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -7,7 +15,13 @@ package cz.muni.pa165.carmanagment;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {        
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"META-INF/applicationContext.xml"});
+         
+        VehicleTypeService service = (VehicleTypeService)context.getBean("vehicleTypeService");
+        service.create(new VehicleTypeDto((long)320));        
+        
+        System.out.println("Size: "+service.findAll().size());   
+        
     }
 }
