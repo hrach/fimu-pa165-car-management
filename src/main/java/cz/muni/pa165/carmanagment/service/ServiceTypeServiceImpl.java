6edf5a5c0ec8d3931874ed0d785db7906d8640cd
Petr Zvonicek <zvonicek@gmail.com>
2013-10-31 10:42:58 +0100

    Added VehicleService and ServiceType implementations

---
+++ src/main/java/cz/muni/pa165/carmanagment/service/ServiceTypeServiceImpl.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.pa165.carmanagment.service;
+
+import cz.muni.pa165.carmanagment.converter.ServiceTypeConverter;
+import cz.muni.pa165.carmanagment.dao.ServiceTypeDaoImpl;
+import cz.muni.pa165.carmanagment.dto.ServiceTypeDto;
+import cz.muni.pa165.carmanagment.model.ServiceType;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author zvonicek
+ */
+public class ServiceTypeServiceImpl implements ServiceTypeService {
+
+    @Autowired
+    private ServiceTypeDaoImpl dao;    
+    
+    @Transactional
+    @Override        
+    public void create(ServiceTypeDto type) {
+        if (type == null)
+            throw new IllegalArgumentException("attribute type is null");
+        
+        ServiceType entity = ServiceTypeConverter.dtoToEntity(type);
+        dao.persist(entity);
+    }
+
+    @Transactional
+    @Override        
+    public void delete(Long id) {
+        if (id == null)
+            throw new IllegalArgumentException("attribute id is null");
+        
+        dao.remove(dao.findById(id));
+    }
+
+    @Transactional
+    @Override        
+    public void update(ServiceTypeDto type) {
+        if (type == null)
+            throw new IllegalArgumentException("attribute type is null");
+        
+        ServiceType entity = ServiceTypeConverter.dtoToEntity(type);
+        dao.update(entity);
+    }
+
+    @Transactional
+    @Override    
+    public ServiceTypeDto findById(Long id) {
+        if (id == null)
+            throw new IllegalArgumentException("attribute id is null");
+        
+        return ServiceTypeConverter.entityToDto(dao.findById(id));
+    }
+
+    @Transactional
+    @Override        
+    public List<ServiceTypeDto> findAll() {
+        return ServiceTypeConverter.entityToDto(dao.findAll());
+    }
+    
+}
---
+++ src/main/java/cz/muni/pa165/carmanagment/service/VehicleServiceImpl.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.pa165.carmanagment.service;
+
+import cz.muni.pa165.carmanagment.converter.RideConverter;
+import cz.muni.pa165.carmanagment.converter.VehicleConverter;
+import cz.muni.pa165.carmanagment.dao.VehicleDaoImpl;
+import cz.muni.pa165.carmanagment.dto.RideDto;
+import cz.muni.pa165.carmanagment.dto.VehicleDto;
+import cz.muni.pa165.carmanagment.model.Vehicle;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author zvonicek
+ */
+@Service
+public class VehicleServiceImpl implements VehicleService {
+
+    @Autowired
+    private VehicleDaoImpl dao;    
+    
+    @Transactional
+    @Override
+    public void create(VehicleDto v) {
+        if (v == null) {
+            throw new IllegalArgumentException("attribute v is null");
+        }
+
+        Vehicle entity = VehicleConverter.dtoToEntity(v);
+        dao.persist(entity);
+    }
+
+    @Transactional
+    @Override    
+    public void delete(Long id) {
+        if (id == null)
+            throw new IllegalArgumentException("attribute id is null");
+        
+        dao.remove(dao.findById(id));
+    }
+    
+    @Transactional
+    @Override
+    public void update(VehicleDto v) {
+        if (v == null)
+            throw new IllegalArgumentException("attribute type is null");
+        
+        Vehicle entity = VehicleConverter.dtoToEntity(v);
+        dao.update(entity);    
+    }
+
+    @Transactional
+    @Override    
+    public VehicleDto findById(Long id) {
+        if (id == null)
+            throw new IllegalArgumentException("attribute id is null");
+        
+        return VehicleConverter.entityToDto(dao.findById(id));    }
+    
+    @Transactional
+    @Override
+    public List<VehicleDto> findAll() {
+        return VehicleConverter.entityToDto(dao.findAll());
+    }
+
+    @Transactional
+    @Override    
+    public List<RideDto> getRidesForVehicle(Long id) {
+        if (id == null)
+            throw new IllegalArgumentException("attribute id is null");        
+        
+        Vehicle v = dao.findById(id);
+        return RideConverter.entityToDto(v.getRides());        
+    }   
+}
