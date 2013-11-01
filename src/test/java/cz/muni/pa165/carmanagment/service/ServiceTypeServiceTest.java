10fce615eae17bb78413b9b500f9b5484b61d755
Petr Zvonicek <zvonicek@gmail.com>
2013-11-01 23:03:35 +0100

    ServiceTypeServiceTest update

--- src/test/java/cz/muni/pa165/carmanagment/service/ServiceTypeServiceTest.java
+++ src/test/java/cz/muni/pa165/carmanagment/service/ServiceTypeServiceTest.java
@@ -8,81 +8,92 @@ import cz.muni.pa165.carmanagment.dao.ServiceTypeDaoImpl;
 import cz.muni.pa165.carmanagment.dto.ServiceTypeDto;
 import cz.muni.pa165.carmanagment.model.ServiceType;
 import java.util.List;
-import static org.mockito.Mockito.*;
 import static junit.framework.Assert.*;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 /**
  *
  * @author tomasbobek
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ServiceTypeServiceTest extends TestCase {
     
-    private ServiceTypeDaoImpl serviceTypeDao;
-    private ServiceTypeServiceImpl serviceTypeService;
+    private ServiceTypeServiceImpl serviceTypeService;    
+    @Mock private ServiceTypeDaoImpl serviceTypeDao;    
     
-    @Override
+    @Before
     public void setUp() throws Exception {
         super.setUp();
         
-        serviceTypeDao = mock(ServiceTypeDaoImpl.class);
         serviceTypeService = new ServiceTypeServiceImpl();
         serviceTypeService.setServiceTypeDao(serviceTypeDao);
     }
-    
-    @Override
-    public void tearDown() throws Exception {
-        super.tearDown();
-    }
-    
-    public void testCreateServiceType() {
-        ServiceTypeDto t = new ServiceTypeDto((long) 1, "Výměna oleje");
-        
-        ServiceType type = serviceTypeService.create(t);
-        
-        assertNotNull(type.getId());
-        assertEquals(t.getId(), type.getId());
-    }
-    
-    public void testGetServiceType() {
-        ServiceTypeDto t = new ServiceTypeDto((long) 1, "Výměna oleje");
-        ServiceTypeDto t1 = new ServiceTypeDto((long) 2, "Výměna brzdových destiček");
-        ServiceTypeDto t2 = new ServiceTypeDto((long) 3, "Kontrola chladící kapaliny");
-        
+
+    @Test
+    public void testCreate() {
+        ServiceTypeDto t = new ServiceTypeDto((long)1, "Výměna oleje");
+                
         serviceTypeService.create(t);
-        serviceTypeService.create(t1);
-        serviceTypeService.create(t2);
         
-        assertEquals(t, serviceTypeService.findById(t.getId()));
-        assertEquals(t1.getId(), serviceTypeService.findById(t1.getId()).getId());
-        assertEquals(t2.getName(), serviceTypeService.findById(t2.getId()).getName());
-        
-        List<ServiceTypeDto> types = serviceTypeService.findAll();
-        assertEquals(3, types.size());
-        assertTrue(types.contains(t));
-        assertTrue(types.contains(t1));
-        assertTrue(types.contains(t2));
+        ArgumentCaptor<ServiceType> captor = ArgumentCaptor.forClass(ServiceType.class);        
+        Mockito.verify(serviceTypeDao)
+               .persist(captor.capture());
+
     }
     
-    public void testUpdateServiceType() {
+    @Test
+    public void testCreateWithNull() {
+        try {
+            serviceTypeService.create(null);
+            fail();
+        } catch (Exception e) {
+            assertEquals(NullPointerException.class, e.getClass());
+        }
+    }    
+    
+    @Test
+    public void testFindAll() {          
+        serviceTypeService.findAll();        
+        Mockito.verify(serviceTypeDao).findAll();
+    }
+    
+    @Test
+    public void testFindById() {        
+        ArgumentCaptor<ServiceType> captor = ArgumentCaptor.forClass(ServiceType.class);
+        serviceTypeService.findById((long)2);
+        
+        Mockito.verify(serviceTypeDao)
+               .findById((long)2);
+    }
+    
+    @Test
+    public void testUpdate() {
+        
         ServiceTypeDto t = new ServiceTypeDto((long) 1, "Výměna oleje");
         
-        serviceTypeService.create(t);
-        t.setName("Výměna brzdových destiček");
+        ArgumentCaptor<ServiceType> captor = ArgumentCaptor.forClass(ServiceType.class);
         serviceTypeService.update(t);
-        ServiceTypeDto t1 = serviceTypeService.findById(t.getId());
         
-        assertEquals(t, t1);
-        assertEquals(t.getName(), t1.getName());
+        Mockito.verify(serviceTypeDao)
+               .update(captor.capture());       
     }
     
-    public void testDeleteServiceType() {
+    @Test
+    public void testDelete() {
         ServiceTypeDto t = new ServiceTypeDto((long) 1, "Výměna oleje");
         
-        ServiceType type = serviceTypeService.create(t);
-        Long typeId = type.getId();
-        serviceTypeService.delete(typeId);
+        ArgumentCaptor<ServiceType> captor = ArgumentCaptor.forClass(ServiceType.class);
+        serviceTypeService.delete(t.getId());
         
-        assertNull(serviceTypeService.findById(typeId));
+        Mockito.verify(serviceTypeDao)
+               .remove(captor.capture());
+
     }
 }
