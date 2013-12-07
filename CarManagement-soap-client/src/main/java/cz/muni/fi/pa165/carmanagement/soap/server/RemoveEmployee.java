dc1d879f185930c64f5301b19856b8ee325a5ede
Petr Zvonicek <zvonicek@gmail.com>
2013-12-07 19:38:50 +0100

    SOAP client module: added autogenerated SOAP management classes
    via JAX-WS RI

---
+++ CarManagement-soap-client/src/main/java/cz/muni/fi/pa165/carmanagement/soap/server/RemoveEmployee.java
@@ -0,0 +1,60 @@
+
+package cz.muni.fi.pa165.carmanagement.soap.server;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for removeEmployee complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="removeEmployee">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="employeeDto" type="{http://server.soap.carmanagement.pa165.fi.muni.cz/}employeeDto" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "removeEmployee", propOrder = {
+    "employeeDto"
+})
+public class RemoveEmployee {
+
+    protected EmployeeDto employeeDto;
+
+    /**
+     * Gets the value of the employeeDto property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link EmployeeDto }
+     *     
+     */
+    public EmployeeDto getEmployeeDto() {
+        return employeeDto;
+    }
+
+    /**
+     * Sets the value of the employeeDto property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link EmployeeDto }
+     *     
+     */
+    public void setEmployeeDto(EmployeeDto value) {
+        this.employeeDto = value;
+    }
+
+}
---
+++ CarManagement-soap-client/src/main/java/cz/muni/fi/pa165/carmanagement/soap/server/RemoveVehicle.java
@@ -0,0 +1,60 @@
+
+package cz.muni.fi.pa165.carmanagement.soap.server;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for removeVehicle complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="removeVehicle">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="vehicleDto" type="{http://server.soap.carmanagement.pa165.fi.muni.cz/}vehicleDto" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "removeVehicle", propOrder = {
+    "vehicleDto"
+})
+public class RemoveVehicle {
+
+    protected VehicleDto vehicleDto;
+
+    /**
+     * Gets the value of the vehicleDto property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link VehicleDto }
+     *     
+     */
+    public VehicleDto getVehicleDto() {
+        return vehicleDto;
+    }
+
+    /**
+     * Sets the value of the vehicleDto property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link VehicleDto }
+     *     
+     */
+    public void setVehicleDto(VehicleDto value) {
+        this.vehicleDto = value;
+    }
+
+}
